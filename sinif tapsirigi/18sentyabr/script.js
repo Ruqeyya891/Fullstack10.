e2af92d687804a4e60176c2c3de31c4dfcbce170
Ruqeyya891 <rugayyara@code.edu.az>
2026-09-18 23:46:01 +0400

    Js start task added

---
+++ sinif tapsirigi/18sentyabr/script.js
@@ -0,0 +1,20 @@
+// task1
+
+let ad = prompt("Adınızı daxil edin");
+let surname = prompt("Soyadınızı daxil edin");
+let age = prompt("Yaşınızı daxil edin");
+
+console.log(ad);
+console.log(surname);
+console.log(age);
+
+// task2
+
+let a = 16; 
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
