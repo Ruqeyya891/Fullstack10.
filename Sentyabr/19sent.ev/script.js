58aa7732d6e8284313e880a8a5252413fa568840
Ruqeyya891 <rugayyara@code.edu.az>
2026-09-19 23:13:13 +0400

    Js hometask added

---
+++ Sentyabr/19sent.ev/script.js
@@ -0,0 +1,31 @@
+// task1
+
+// 3imtahan balinin ortalamasini hesablayin
+
+let score1 = 50;
+let score2 = 70;
+let score3 = 90;
+let average = ((score1 + score2 + score3) / 3);
+
+console.log("average: " + average);
+
+// task2
+
+let bankBalance = 9000;
+let add = 500;
+let withdrawal = 200;
+let newBalance = bankBalance + add - withdrawal;
+let amount = newBalance * 2 / 5;
+let square = amount ** 2;
+let result = square % 7;
+
+console.log("Qalıq: " + result);
+
+// task 3
+
+let headphonesPrice = 75;
+let orderTotal = headphonesPrice * 4;
+let userPaid = 250;
+let debt = orderTotal - userPaid;
+
+console.log("Borc: " + debt);
