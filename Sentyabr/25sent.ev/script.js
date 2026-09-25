f8f266c8c70ea2ef8491207cbe6a5fa4ca19a2bc
Ruqeyya891 <rugayyara@code.edu.az>
2026-09-25 08:27:54 +0400

    array-hometask added

---
+++ Sentyabr/25sent.ev/script.js
@@ -0,0 +1,3 @@
+let colorArray = ["red", "green", "blue", "yellow", "purple", "orange", "pink", "brown", "black", "white"];
+let randomIndex = Math.floor(Math.random() * colorArray.length);
+console.log(colorArray[randomIndex]);
